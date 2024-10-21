4e934c1f2cefe08158e3ce78bfe73ba9590beedc
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-10-21 14:53:29 +0300

    базовый сценарий загрузки

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useAppDispatch, useAppSelector } from './store/hooks/redux';
 import { userSlice } from './store/reducers/UserSlice';
+import { fetchUsers } from './store/reducers/ActionCreators';
 
 const App = () => {
-  const { count } = useAppSelector((state) => state.userReducer);
-  const { increment } = userSlice.actions;
   const dispatch = useAppDispatch();
+  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
 
-  
+  useEffect (() => {
+    dispatch(fetchUsers())
+  }, [])
 
   return (
     <div className='App'>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment(10))}>Increment</button>
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>Error</h1>}
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 };

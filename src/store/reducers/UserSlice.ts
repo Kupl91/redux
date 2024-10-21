1d8532f883f567fbdee1c39676d0f15cb2e1f7bf
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-10-21 14:17:13 +0300

    first push
    rtk for new players

---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,30 @@
+import { error } from "console";
+import { IUser } from "../../models/IUser"
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+    users: IUser[];
+    isLoading: boolean;
+    error: string;
+    count: number;
+}
+
+
+const initialState: UserState ={
+    users: [],
+    isLoading: false,
+    error: '',
+    count: 25
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        increment(state, action: PayloadAction<number>) {
+            state.count += action.payload
+        }
+    }
+})
+
+export default userSlice.reducer;
